28f7da0ab19d6feae44eaea28ceaf037cfc8ca7d
Emanuel Posescu <kind3r@gmail.com>
2025-10-17 06:34:29 +0300

    Update reward condition.

--- src/reward.ts
+++ src/reward.ts
@@ -1,18 +1,35 @@
 export interface BiqRewardCondition {
   id: string;
+  clientId: string;
+  organizerId: string;
   event: string;
   location?: string;
   area?: string;
   recurrence: BiqRewardRecurrence;
+  /** Minutes of participation */
   requiredParticipation: number;
   validFrom?: number;
   validUntil?: number;
+  /** Require previously obtained rewards */
+  requiredRewards?: BiqRequiredRewards;
 }
 
 export enum BiqRewardRecurrence {
   ONCE = "ONCE",
   REPEATING = "REPEATING",
 }
+
+export interface BiqRequiredReward {
+  id: string;
+  optional: boolean;
+}
+
+export interface BiqRequiredRewards {
+  /** Number of required optional rewards */
+  optionalCount: number;
+  rewards: BiqRequiredReward[];
+}
+
 
 export interface BiqReward {
   id: string;
